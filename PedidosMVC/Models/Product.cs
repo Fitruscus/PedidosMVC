7399f64e9f07fc6e2c1bba097f11c09e4e0b9b39
Fitruscus <adolfovera97@gmail.com>
2025-09-18 19:13:42 -0400

    Validaciones Product

--- PedidosMVC/Models/Product.cs
+++ PedidosMVC/Models/Product.cs
@@ -1,10 +1,20 @@
-﻿namespace PedidosMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PedidosMVC.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre no puede superar los 100 caracteres")]
         public string Nombre { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser positivo")]
         public decimal Precio { get; set; }
+
+        [Required(ErrorMessage = "El stock es obligatorio")]
+        [Range(0, int.MaxValue, ErrorMessage = "El stock debe ser mayor o igual a 0")]
         public string Stock { get; set; }
     }
 }
