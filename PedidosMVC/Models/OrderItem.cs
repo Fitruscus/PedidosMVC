b50f105623ea93f87355656c881957f964c7b80b
Fitruscus <adolfovera97@gmail.com>
2025-09-18 19:24:18 -0400

    Validación OrderItem

--- PedidosMVC/Models/OrderItem.cs
+++ PedidosMVC/Models/OrderItem.cs
@@ -1,10 +1,18 @@
-﻿namespace PedidosMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PedidosMVC.Models
 {
     public class OrderItem
     {
         public int Id { get; set; }
+
+        [Required]
         public int Producto { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 0")]
         public int Cantidad { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "El subtotal debe ser positivo")]
         public int Subtotal { get; set; }
     }
 }
